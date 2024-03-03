b7beb6ecac0c4e005191bc896148d2a140efa30b
Mori Adrian Rosland <mori@live.no>
2024-03-03 13:46:26 +0100

    feat: add buffer size constant

--- src/network/receiver.go
+++ src/network/receiver.go
@@ -6,20 +6,22 @@ import (
 	"time"
 )
 
+BUFFER_SIZE := 1024
+
 /*
 Listen for incoming messages on specified IP and port.
 */
 func ListenForMessages(ip string, port int, messageChannelchan chan<- []byte) {
-	conn, err := net.ListenPacket("udp4", ip+":"+strconv.Itoa(port))
+	conn, err := reuseport.ListenPacket("udp4", ip+":"+strconv.Itoa(port))
 
 	if err != nil {
 		messageChannelchan <- []byte("CONNECTION ERROR")
-		return
+		panic(err)
 	}
 
 	defer conn.Close()
 
-	buffer := make([]byte, 1024)
+	buffer := make([]byte, BUFFER_SIZE)
 
 	for {
 		_, _, err := conn.ReadFrom(buffer)
@@ -29,29 +31,5 @@ func ListenForMessages(ip string, port int, messageChannelchan chan<- []byte) {
 		}
 
 		messageChannelchan <- buffer
-	}
-}
-
-// ----- TEST FUNCTION ----- //
-
-/*
-Sends a message on specified IP and port every 5 seconds (debug function).
-*/
-func SendUDPMessages(ip string, port int) {
-	conn, err := net.Dial("udp", ip+":"+strconv.Itoa(port))
-	if err != nil {
-		panic(err)
-	}
-	defer conn.Close()
-
-	message := []byte("Oh, so you wanna talk about the non-local quantum hologram, the phase-conjugate adaptive waves resonating in micro-tubules in the brain, which of course requires some closed-timelike curves and Lorentzian manifold, and... you'll catch up, I'll wait.")
-
-	for {
-		_, err = conn.Write(message)
-		if err != nil {
-			panic(err)
-		}
-
-		time.Sleep(5 * time.Second)
 	}
 }
