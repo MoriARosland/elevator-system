8f599f00fba0e2c6d9a4188dcb67fd9d3bd5c9f9
Mori Rosland MAC <moriar@stud.ntnu.no>
2024-03-05 15:51:10 +0100

    feat: modify Requests to 3d array

--- src/types/types.go
+++ src/types/types.go
@@ -35,7 +35,7 @@ type ElevState struct {
 	Floor           int
 	Dirn            elevio.MotorDirection
 	DoorObstr       bool
-	Requests        [][]bool
+	Requests        [][][]bool
 	NextNode        NextNode
 	WaitingForReply bool
 }
