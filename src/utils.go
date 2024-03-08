f5e074678949a19a389c0c82a6ef2eaa6ee2c8e4
Aasmund Nørsett <aasmund.norsett@gmail.com>
2024-03-08 18:56:18 +0100

    refactor: create main package utils file

---
+++ src/utils.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+/*
+ * Parse command line arguments
+ */
+func parseCommandlineFlags() (int, int, int, int) {
+	nodeID := flag.Int("id", -1, "Node id")
+	numNodes := flag.Int("num", -1, "Number of nodes")
+	baseBroadcastPort := flag.Int("bport", -1, "Base Broadcasting port")
+	elevServerPort := flag.Int("sport", -1, "Elevator server port")
+
+	flag.Parse()
+
+	if *nodeID < 0 || *numNodes < 0 || *baseBroadcastPort < 0 || *elevServerPort < 0 {
+		fmt.Println("Missing flags, use flag -h to see usage")
+		os.Exit(1)
+	}
+
+	return *nodeID, *numNodes, *baseBroadcastPort, *elevServerPort
+}
